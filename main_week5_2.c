3879d971df16667df1a81239f9dbf0269cbb7987
cyj4972 <144991384+cyj4972@users.noreply.github.com>
2023-10-05 16:24:57 +0900

    week5_4

---
+++ main_week5_2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int sum = 0;
+	int num;
+	int i;
+	
+	printf("input a number : ");
+	scanf("%d", &num);
+	
+	for(i=0; i<=num; i++)
+	{
+		sum = sum + i;
+	}
+	printf("the result is %d", sum);
+	
+	system("PAUSE");
+	return 0;
+}
